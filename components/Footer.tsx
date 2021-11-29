4b6c92106378714c8918103a7d7a5ac0e46bd57d
ivyxjc <ivyxjc1994@hotmail.com>
2021-11-29 16:59:22 +0800

    feat(): update web footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -21,6 +21,9 @@ const Footer = () => {
           <a className="hover:text-white" href="https://www.typescriptlang.org/">
             TypeScript.
           </a>
+          <a className="hover:text-white" href="https://beian.miit.gov.cn/">
+            浙ICP备19039496号-1
+          </a>
         </div>
         <div>Ivyxjc © 2016-{currentYear}</div>
       </div>
